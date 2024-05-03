74dc689f3cec9292f70c36800213d8040e9ad3e0
graceK-10 <kipagrace@gmail.com>
2024-05-03 10:10:27 +0200

    dice images

--- dice.js
+++ dice.js
@@ -1,5 +1,43 @@
 function rollDice() {
-    window.location.href = "loader.html";
+
+    // F U N C T I O N   T O   R O L L  T H E   D I C E   
+    let diceNum1 = document.querySelector(".img1");
+    let diceNum2 = document.querySelector(".img2");
+
+  //   r o l l i n g   d i c e s   
+    diceNum1.setAttribute("src", "images/dice-3.jpeg")
+    diceNum2.setAttribute("src", "images/dice-4.jpeg")
+
+
+    let result = document.querySelector('h2')
+    result.innerHTML = ""
+
+//   //  Try catch block to handle errors 
+//     try {
+//     setTimeout(() => {
+//         let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+//         let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+//         diceNum1.setAttribute('src', 'images/dice' + randomNumber1 + '.jpeg');
+//         diceNum2.setAttribute('src', 'images/dice' + randomNumber2 + '.jpeg');
+
+//         //determine the winner
+//         if (randomNumber1 === randomNumber2) {
+//             result.innerHTML = "Draw!"
+//         }
+//         else if (randomNumber1 < randomNumber2) {
+//             result.innerHTML = <span class='winner-name'> + player2Name + </span> + "Won! ";
+//         }
+//         else {
+//             result.innerHTML = <span class='winner-name'> + player1Name + </span> + "Won! ";
+//         }
+
+//     }, 2500);
+
+// } catch(error) {
+//   // Displays error message 
+//   console.log(error.message);
+// }
 }
 
 function goBack() {
